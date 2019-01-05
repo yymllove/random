c474d30353d730623ce20a2c3536702e4c65f136
yaoyuan <yaoyuan@4paradigm.com>
2019-01-05 22:46:15 +0800

    Updates

---
+++ precache-manifest.ab3cca7c8e6574369b6d4a40f38646c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f80ae3bf1cbcda5dad1",
+    "url": "/random/static/js/runtime~main.5f80ae3b.js"
+  },
+  {
+    "revision": "87387e740492b3002b5a",
+    "url": "/random/static/js/main.87387e74.chunk.js"
+  },
+  {
+    "revision": "0a763d2c0887d1d2aa91",
+    "url": "/random/static/js/1.0a763d2c.chunk.js"
+  },
+  {
+    "revision": "87387e740492b3002b5a",
+    "url": "/random/static/css/main.ff94dcf9.chunk.css"
+  },
+  {
+    "revision": "3b09c813b54f22b0792d801d32d364e8",
+    "url": "/random/index.html"
+  }
+];
